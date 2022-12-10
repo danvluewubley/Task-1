a4872fde58580a7d35b7450fea78ddd47a63e5df
danvluewubley <119687718+danvluewubley@users.noreply.github.com>
2022-12-09 21:52:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import socket
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 9999
+
+
+class NeuralHTTP(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header("Content-type", "text/html")
+    self.end_headers()
+
+    self.wfile.write(bytes("<html><body><h1>HELLO WORLD!</h1></body></html>", "utf-8"))
+
+def do_POST(self):
+  self.send_response(200)
+  self.send_header("Content-type", "application/json")
+  self.end_headers()
+
+
+server = HTTPServer((HOST, PORT), NeuralHTTP)
+print("Server now running...")
+server.serve_forever()
+server.server_close()
+print("Server stopped!")
